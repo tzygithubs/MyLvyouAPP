e816b5a8d5847dca48406865071716bb1d45022f
T <503601228@qq.com>
2017-07-11 15:53:35 +0800

    第四次

--- app/src/main/java/main/dlxy/com/mylvyouapp/MainActivity.java
+++ app/src/main/java/main/dlxy/com/mylvyouapp/MainActivity.java
@@ -9,9 +9,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    //不出去fffff
-        //fghdfg
-        //asd
-        //asds
+
     }
 }
