de1ab6a67d421fcd707ef7681d887e73b49066c6
T <503601228@qq.com>
2017-07-11 23:23:16 +0800

    第七次

--- app/src/main/java/main/dlxy/com/mylvyouapp/MainActivity.java
+++ app/src/main/java/main/dlxy/com/mylvyouapp/MainActivity.java
@@ -1,14 +1,35 @@
 package main.dlxy.com.mylvyouapp;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.widget.RadioGroup;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager vp;
+    private RadioGroup rg;
+    private List<Fragment> fragmentlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainactivity);
+        initview();
+    }
+
+    private void initview() {
+        vp =findViewById(R.id.viewpager_main);
+        rg = findViewById(R.id.radio_main);
+        findViewById(R.id.radiobutton_shouye);
+        findViewById(R.id.radiobutton_xingcheng);
+        findViewById(R.id.radiobutton_faxian);
+        findViewById(R.id.radiobutton_kefu);
+        findViewById(R.id.radiobutton_wode);
+        fragmentlist = new ArrayList<Fragment>();
 
     }
 }
