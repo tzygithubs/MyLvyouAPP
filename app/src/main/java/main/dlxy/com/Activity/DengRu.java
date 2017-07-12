f0a46f06e31e034d848000defdbb395c74323dc8
T <503601228@qq.com>
2017-07-12 16:28:30 +0800

    tzy 第15次

--- app/src/main/java/main/dlxy/com/Activity/DengRu.java
+++ app/src/main/java/main/dlxy/com/Activity/DengRu.java
@@ -1,9 +1,16 @@
 package main.dlxy.com.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dlxy.Utils.VolleyUtil;
+import com.dlxy.interfaces.LoginCallBack;
 
 import main.dlxy.com.mylvyouapp.R;
 
@@ -11,11 +18,50 @@ import main.dlxy.com.mylvyouapp.R;
  * Created by T on 2017/7/12.
  */
 
-public class DengRu extends Activity{
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.dengru_layout);
+public class DengRu extends Activity implements View.OnClickListener {
+    private Button dr;
+    private EditText zh , ma ;
+    private TextView zc;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dengru_layout);
+        initView();
+    }
+
+
+    private void initView() {
+        dr =(Button) findViewById(R.id.bt_dengru);
+        zh = findViewById(R.id.et_name);
+        ma = findViewById(R.id.et_password);
+        zc =(TextView) findViewById(R.id.text_zhuce);
+        dr.setOnClickListener(this);
+        zc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_dengru:
+                String names = zh.getText().toString().trim();
+                String password = ma.getText().toString().trim();
+
+                VolleyUtil.getInstance().login(names, password, new LoginCallBack() {
+                    @Override
+                    public void success(String info) {
+                        Intent intent = new Intent(DengRu.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void errr(String error) {
+                        Toast.makeText(DengRu.this, "账户或密码错误", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                break;
+            case R.id.text_zhuce:
+                break;
+        }
     }
 }
