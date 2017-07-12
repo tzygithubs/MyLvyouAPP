e537a68c1e509c284839cea45e67f17ef2be9105
T <503601228@qq.com>
2017-07-12 11:05:25 +0800

    第11次

---
+++ app/src/main/java/com/dlxy/Utils/VolleyUlist.java
@@ -0,0 +1,8 @@
+package com.dlxy.Utils;
+
+/**
+ * Created by T on 2017/7/12.
+ */
+
+public class VolleyUlist  {
+}
--- app/src/main/java/com/dlxy/domain/Customer.java
+++ app/src/main/java/com/dlxy/domain/Customer.java
@@ -10,4 +10,55 @@ public class Customer {
     private String address;
     private String phone;
 
+    public Customer() {
+    }
+
+    public Customer(String name, int id, String address, String phone) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
